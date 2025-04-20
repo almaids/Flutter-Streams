969b38cffd0342d1920228cbe48fbfb3e1d6e134
almaids <150983302+almaids@users.noreply.github.com>
2025-04-20 18:16:21 +0700

    P1: Jawaban Soal 2

---
+++ stream_almaids/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.pink,
+    Colors.pinkAccent,
+    Color(0xFFF8BBD0), 
+    Color(0xFFD81B60), 
+    Color(0xFFFFC1E3), 
+  ];
+}
